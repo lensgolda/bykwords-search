a8042b7b959b05b961c2ec1fbca89d3ef1369e61
Alexey Golda <lensgolda@gmail.com>
2017-10-05 00:51:08 +0200

    update librarries to new versions; add wrap-reload

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-defaults "0.2.3"]
-                 [compojure "1.5.2"]
+                 [ring/ring-defaults "0.3.1"]
+                 [compojure "1.6.0"]
                  [ring/ring-json "0.4.0"]
                  [http-kit "2.2.0"]
+                 [clj-http "3.7.0"]
                  [hiccup "1.0.5"]
                  [org.clojure/data.xml "0.0.8"]
-                 [org.clojure/data.zip "0.1.2"]
-                 [cheshire "5.7.0"]]
-  :profiles {:dev {:dependencies [[ring/ring-devel "1.5.1"]]}}
+                 [org.clojure/data.zip "0.1.2"]]
+                 [cheshire "5.8.0"]]
+  :profiles {:dev {:dependencies [[ring/ring-devel "1.6.2"]]}}
   :main bykwords-search.core/-main
   :aot [bykwords-search.core])
